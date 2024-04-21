2702630a9669f4c62a500bfb093bd74b2570f5c9
RidaSeqq <122315165+RIDASEQQAM@users.noreply.github.com>
2024-04-21 03:02:58 +0100

    test1

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import "./app.scss"
+import Navbar from "./components/navbar/navbar";
+
 const App = () => {
-  return <div>Hello World</div>;
+  return <div>
+    <section>
+      <Navbar/>
+    </section>
+    <section>Parallax</section>
+    <section>Service</section>
+    <section>Parallax</section>
+    <section>Potfolio1</section>
+    <section>Potfolio2</section>
+    <section>Potfolio3</section>
+    <section>Contact</section>
+  </div>;
 };
 
 export default App;
